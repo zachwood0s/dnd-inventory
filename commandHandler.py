f424a9e8ca04e99b6bff194529afac8c5ae3d6c7
Zach Woods <zach@hayzak.com>
2021-07-14 00:05:44 -0500

    No crash on bad command

--- commandHandler.py
+++ commandHandler.py
@@ -21,24 +21,28 @@ def register_command(cmd_name: str, n_args: int, help_text: str):
 
 
 def parse_command(input_str: str):
-    print(f'Received command {input_str}')
-    input_str = input_str.strip()
-    command = input_str.split(' ')
+    try:
+        print(f'Received command {input_str}')
+        input_str = input_str.strip()
+        command = input_str.split(' ')
 
-    commands = _REGISTERED_COMMANDS.get(command[0].lower())
-    arg_cnt = len(command) - 1
-    if commands is not None:
-        handler = commands.get(arg_cnt)
-        if handler is not None:
-            try:
-                handler.func(command)
-            except Exception as e:
-                print(e)
-                raise e
+        commands = _REGISTERED_COMMANDS.get(command[0].lower())
+        arg_cnt = len(command) - 1
+        if commands is not None:
+            handler = commands.get(arg_cnt)
+            if handler is not None:
+                try:
+                    handler.func(command)
+                except Exception as e:
+                    print(e)
+                    raise e
+            else:
+                print(f'No command "{command[0]}" that takes {arg_cnt} arg(s) found')
+                print('Options:')
+                for cmd in commands.values():
+                    print(cmd.help_text)
         else:
-            print(f'No command "{command[0]}" that takes {arg_cnt} arg(s) found')
-            print('Options:')
-            for cmd in commands.values():
-                print(cmd.help_text)
-    else:
-        print(f'Command "{command[0]}" not found')
+            print(f'Command "{command[0]}" not found')
+
+    except Exception as e:
+        print(e)
